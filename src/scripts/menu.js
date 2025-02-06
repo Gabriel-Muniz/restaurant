deb3e26249b7ecfdabee6ac94092de53f9db4310
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2025-02-05 22:15:45 -0300

    STYLE: add dishes image

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,16 +1,20 @@
 import "../css/menu.css"
+import parmegianaPhoto from '../assets/dishes/parmegiana.jpg';
+import strogonoffPhoto from '../assets/dishes/strogonoff.jpg';
+import carbonaraPhoto from '../assets/dishes/carbonara.jpg';
 
 const content = document.getElementById("content");
 
 const pageTitle = document.createElement("h1");
 pageTitle.textContent = "Menu";
 
-const createDishe = (title, about, price) => {
+const createDishe = (photo, title, about, price) => {
   const divRow = document.createElement("div");
   divRow.classList.add("div-row");
 
   const dishePhoto = document.createElement("div");
   dishePhoto.classList.add("photo-dishe");
+  dishePhoto.style.backgroundImage = `url(${photo})`
 
   const disheInfo = document.createElement("div");
   disheInfo.classList.add("info-dishe");
@@ -36,12 +40,14 @@ const createDishe = (title, about, price) => {
 };
 
 const parmediana = createDishe(
+  parmegianaPhoto,
   "ParmeDiana",
   `ParmeDiana is a dishe made in homage for the first woman soccer player from Palmeiras. Princess Diana`,
   "R$ 19,14"
 );
 
 const strogonoff = createDishe(
+  strogonoffPhoto,
   "StruggleOff",
   `It's a really hard dishe to make, we don't have the ingredients... So
             please, don't order this one...`,
@@ -49,6 +55,7 @@ const strogonoff = createDishe(
 );
 
 const carbonara = createDishe(
+  carbonaraPhoto,
   "Carbonara",
   `That's a good one to order if you're feeling lucky.
             The chance of getting a carbonized one is really high.
